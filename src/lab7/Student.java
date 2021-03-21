35a4cd825e7606d7a49fdaed69752ce973fe3317
Hani Home <hmthubaiti@uqu.edu.sa>
2021-03-21 12:18:03 +0300

    update file

--- src/lab7/Student.java
+++ src/lab7/Student.java
@@ -16,9 +16,6 @@ public class Student {
 
     public String highestGrade() {
         //write your algorithm here
-        if (true) {
-
-        }
         return "Your higher score in Physics (96)";
     }
 
